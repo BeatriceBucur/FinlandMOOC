fd413bd73ccfa5799e6bfc2e5d4c9fa7f78f0de0
Beatrice Bucur <beatrice.bucur@gmail.com>
2019-08-05 21:47:41 +0300

    added correct password randomizer Files

--- src/main/java/week4/passwordRandomizer/Randomizer.java
+++ src/main/java/week4/passwordRandomizer/Randomizer.java
@@ -3,7 +3,7 @@ package week4.passwordRandomizer;
 public class Randomizer {
     public static void main(String[] args) {
 
-            PasswordRandomizer randomizer = new PasswordRandomizer(13);
+            PasswordRandomizer randomizer =  new PasswordRandomizer(13);
             System.out.println("Password: " + randomizer.createPassword());
             System.out.println("Password: " + randomizer.createPassword());
             System.out.println("Password: " + randomizer.createPassword());
